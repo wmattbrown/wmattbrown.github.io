9a2c169a894c39c962650f6f8f52fb3369e0599c
Matthew Brown <w.matthew.brown@gmail.com>
2021-04-11 15:52:59 -0400

    updating journal entries

---
+++ delete_old_copy_new.R
@@ -0,0 +1,13 @@
+# script to delete contents of this folder and replace it with the contents of the vernon_gitbook docs/ folder
+
+# delete the folders and everything in them
+system("rm -r assets")
+system("rm -r css")
+system("rm -r media")
+# list.files automatically doesn't include files starting with . so .gitignore isn't includes
+files <- list.files()
+files_to_delete <- files[!grepl("Rproj|delete_old_copy_new", files)]
+file.remove(files_to_delete)
+
+# now there are only the .gitignore file and the wmattbrown.github.io.Rproj files
+
